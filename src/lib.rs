22a0957a854221744dfb7fc4b42ca3b58d971c1c
Jyotsna Prakash <jyotsna.prakash@gmail.com>
2017-07-08 10:27:29 -0700

    iterate thru cmdline args instead of using clone

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
 
         Ok(Config {
             query: query,
